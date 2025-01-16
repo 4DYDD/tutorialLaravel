545804da0af1078ef59a1b28f636182cc1eb8054
Haidir Aditya <haidiraditya16@gmail.com>
2025-01-16 08:13:59 +0800

    belajar laravel 11 part 4 dengan Sandhika Galih

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// home
+// blog
+// about
+// contact
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
