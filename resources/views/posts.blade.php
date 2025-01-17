3d3d6b6973d774c236a86f46667df076dd418df4
Haidir Aditya <haidiraditya16@gmail.com>
2025-01-17 18:02:15 +0800

    rehat bentar soalnya hari begerugum, dalam tahap lanjut materi Belajar laravel 11 part 6 dengan Sandhika Galih

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    @foreach ($posts as $post)
+        <article class="max-w-screen-md py-8 border-b border-gray-300">
+            <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">
+                <a class="hover:underline" href="/posts/{{ $post['id'] }}">
+                    {{ $post['title'] }}
+            </h2>
+            </a>
+            <div class="text-base text-gray-500"><a href="#">{{ $post['author'] }}</a> | 17 Januari 2025</div>
+            <p class="my-4 font-light">
+                {{ Str::limit($post['body'], 150) }}
+            </p>
+            <a class="font-medium text-blue-500 hover:underline" href="/posts/{{ $post['id'] }}">Read More &raquo;</a>
+        </article>
+    @endforeach
+
+
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,17 +13,59 @@ Route::get('/', function () {
 // contact
 
 Route::get('/home', function () {
-    return view('home', ["title" => "home"]);
+    return view('home', ["title" => "Home"]);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ["title" => "blog"]);
+Route::get('/posts', function () {
+    return view('posts', ["title" => "Blog", "posts" => [
+        [
+            'id' => '1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Haidir Aditya',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic consequuntur perspiciatis nam ipsum eveniet
+            repudiandae asperiores quae ea modi mollitia!'
+        ],
+        [
+            'id' => '2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Haidir Aditya',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur non quidem perspiciatis officia vero
+        impedit saepe exercitationem neque repudiandae nisi, similique maxime amet tenetur fugit!'
+        ],
+    ]]);
 });
+
+Route::get('/posts/{id}', function ($id) {
+    // dd($id);
+    $posts = [
+        [
+            'id' => '1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Haidir Aditya',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic consequuntur perspiciatis nam ipsum eveniet
+            repudiandae asperiores quae ea modi mollitia!'
+        ],
+        [
+            'id' => '2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Haidir Aditya',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur non quidem perspiciatis officia vero
+        impedit saepe exercitationem neque repudiandae nisi, similique maxime amet tenetur fugit!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    dd($post);
+});
+
 
 Route::get('/about', function () {
-    return view('about', ["title" => "about", "name" => "Haidir Aditya"]);
+    return view('about', ["title" => "About", "name" => "Haidir Aditya"]);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ["title" => "contact"]);
+    return view('contact', ["title" => "Contact"]);
 });
